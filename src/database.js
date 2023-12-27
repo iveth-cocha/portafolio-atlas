97f4f060db41f4e1c4c8bea0dffeef111feed07f
iveth-cocha <maria.iveth16@outlook.es>
2023-12-26 21:26:54 -0500

    conexion a atlas

--- src/database.js
+++ src/database.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 
 
-const MONGODB_URI = 'mongodb://localhost:27017/portfolio'
+//const MONGODB_URI = 'mongodb://localhost:27017/portfolio'
+//atlas
+const MONGODB_URI = 'mongodb+srv://portafolio:portafolio@cluster0.msoyex9.mongodb.net/?retryWrites=true&w=majority'
+
 
 connection = async()=>{
     try {
          await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Database is connected")
+        console.log("Database is connected to Mongo DB Atlas")
     } catch (error) {
         console.log(error);
     }
